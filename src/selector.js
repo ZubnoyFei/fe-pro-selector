cd69a1b9d96a13a245eb4b8afd427e581a6e539e
Oleksii Ustinov <ustinovoleksij@gmail.com>
2022-01-04 10:19:17 +0200

    Added the selecotr function

--- src/selector.js
+++ src/selector.js
@@ -1,5 +1,18 @@
-const selector = (obj, keys) => {
-    // Ваше решение должно быть здесь
-};
+function selector(obj, path){
+let currentObj = obj;
 
-module.exports = selector;
+path.forEach((key) => {
+if(currentObj[key]) {
+    currentObj = currentObj[key];
+}
+});
+return currentObj;
+}
+
+const obj = {
+    field : {
+        test: 'Oleksii'
+    }
+}
+
+console.log(selector (obj['field','test']));
